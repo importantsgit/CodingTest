d71d4096f96b8b9629f1940c77f0d8dd908238c6
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-01-20 15:00:18 +0900

    LinkList

--- DataStructure/LinkedList/Doubly LinkedList.playground/Contents.swift
+++ DataStructure/LinkedList/Doubly LinkedList.playground/Contents.swift
@@ -1,3 +1,4 @@
+
 class Node<T> {
     var prev: Node?
     var data: T?
@@ -13,4 +14,75 @@ class Node<T> {
 class DoublylinkedList<T:Equatable> {
     private var head: Node<T>?
     private var tail: Node<T>?
+    
+    // tail은 늘 마지막 노드 (새로운 노드 추가시 tail에 추가)
+    func append(data : T?) {
+        if head == nil {
+            head = Node.init(data: data)
+            tail = head
+        }
+        
+        let newNode = Node.init(data: data)
+        tail?.next = newNode
+        newNode.prev = tail
+        tail = newNode
+    }
+    
+    func insert(data: T?, at index: Int) {
+        
+        // head가 없는 경우 Node를 생성 후 head로 지정한다.
+        if head == nil {
+            print("링크 리스트에 노드가 전혀 없습니다.")
+            return
+        }
+        
+        var node = head
+        for _ in 0 ..< (index - 1) { // 입력된 인덱스 전까지 올라감
+            if node?.next == nil {
+                print("인덱스 범위를 넘어갔습니다.")
+                return
+            }
+            node = node?.next
+        }
+            
+        // change node
+        let nextNode = node?.next
+        node?.next = Node(data: data)
+        node?.next?.prev = node
+        node?.next?.next = nextNode
+        nextNode?.prev = node?.next
+    }
+    
+    func removeLast() {
+        if head == nil {
+            print("링크 리스트에 노드가 전혀 없습니다.")
+            return
+        }
+        
+        // head를 삭제하는 경우 (연결 리스트에 노드가 1개 밖에 없는 경우)
+        if head?.next == nil {
+            head = nil
+            tail = nil
+            return
+        }
+        
+        tail?.prev?.next = nil
+        tail = tail?.prev
+    }
+    
+    func remove(at index: Int) {
+        if head == nil {
+            print("현재 저장된 노드가 전혀 없습니다.")
+            return
+        }
+        
+        var node = head
+        for _ in 0 ..< index-1 {
+            if node?.next?.next == nil {break}
+            node = node?.next
+        }
+        
+        node?.next = node?.next?.next
+        // 가운데 노드 연결 끊기
+    }
 }
--- DataStructure/LinkedList/LinkedList.playground/Contents.swift
+++ DataStructure/LinkedList/LinkedList.playground/Contents.swift
@@ -10,11 +10,13 @@ class Node<T> {
 
 class LinkedList<T:Equatable> {
     private var head: Node<T>?
+    var count: Int = 0
     
     func append(data: T?) {
         // 만약 head가 없으면 노드 추가
         if head == nil {
             head = Node(data: data)
+            count += 1
             return
         }
         
@@ -25,6 +27,7 @@ class LinkedList<T:Equatable> {
         }
         // 맨 마지막 노드에 연결
         node?.next = Node(data: data)
+        count += 1
         
     }
     
@@ -40,34 +43,51 @@ class LinkedList<T:Equatable> {
         for _ in 0 ..< (index - 1) { // 입력된 인덱스 전까지 올라감
             if node?.next == nil {
                 print("인덱스 범위를 넘어갔습니다.")
-                break
+                return
             }
-            
-            // change node
-            let nextNode = node?.next
-            node?.next = Node(data: data)
-            node?.next?.next = nextNode
+            node = node?.next
+            count += 1
         }
+            
+        // change node
+        let nextNode = node?.next
+        node?.next = Node(data: data)
+        node?.next?.next = nextNode
     }
     
-    func removeLast(){
+    func popLast()->T?{
         if head == nil {
             print("저장된 노드가 없습니다.")
-            return
+            return nil
         }
         
         var node = head
+        
+        // head를 삭제하는 경우
+        if node?.next == nil {
+            head = nil
+            return node?.data
+        }
+        
         // 만약 현재 노드의 next 노드의 next가 nil이라면 다음 노드가 마지막임으로 현재
-        while node?.next?.next == nil {
+        while node?.next?.next != nil {
             node = node?.next
         }
+        let data = node?.next?.data
         node?.next = nil
+        count -= 1
+        return data
     }
     
-    func remove(at index: Int) {
+    func pop(at index: Int)->T? {
+        if count-1 < index {
+            print("노드리스트 범위를 벗어났습니다.")
+            return nil
+        }
+        
         if head == nil {
             print("현재 저장된 노드가 전혀 없습니다.")
-            return
+            return nil
         }
         
         var node = head
@@ -75,9 +95,11 @@ class LinkedList<T:Equatable> {
             if node?.next?.next == nil {break}
             node = node?.next
         }
-        
+        let data = node?.next?.data
         node?.next = node?.next?.next
         // 가운데 노드 연결 끊기
+        count -= 1
+        return data
     }
     
     func searchFirstNum(from data: T?) -> Node<T>? {
@@ -111,12 +133,21 @@ class LinkedList<T:Equatable> {
 }
 
 var linkList: LinkedList = LinkedList<Int>()
+
 linkList.append(data: 1)
 linkList.append(data: 5)
 linkList.append(data: 4)
 linkList.append(data: 3)
 linkList.append(data: 2)
 
-linkList.remove(at: 2)
+print(linkList.returnArray()!)
+
+print(linkList.pop(at: 2))
 
 print(linkList.returnArray()!)
+
+print(linkList.count)
+
+for _ in 0..<7 {
+    print(linkList.popLast())
+}
