578f0f3896469971810cd60361ad1e49c2ced193
importantsgit <dlwognsdk@tukorea.ac.kr>
2023-02-10 16:01:49 +0900

    and

--- BaekJoon_ algorithm/DP/25644.playground/Contents.swift
+++ BaekJoon_ algorithm/DP/25644.playground/Contents.swift
@@ -6,9 +6,13 @@ var arr = [1,4,7,2,6]
 func maxUP(_ arr: [Int]) {
     var DP = Array(repeating: 0, count: arr.count)
     var maxNums:[Int] = []
-    for i in 1..<DP.count-1 {
-            DP[j] = (arr[j]-arr[j-1])+DP[j-1]-DP[i-1]
-        print(DP)
+    for i in 1..<DP.count {
+            DP[i] = (arr[i]-arr[i-1])+DP[i-1]
     }
+    maxNums.append(DP.max())
+    for i in 0..<DP.count {
+        
+    }
+    print(DP)
 }
 maxUP(arr)
